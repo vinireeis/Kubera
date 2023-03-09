3520fd9e8b9bd3ad0196e31fe6934d0041a519b0
Vinicius Reis <v_reis@outlook.com.br>
2023-03-09 16:07:10 -0300

    feat: implemented register router and run server in main app

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import uvicorn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from src.routers.base.router import BaseRouter
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+app = BaseRouter.register_routers()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
