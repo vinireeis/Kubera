dc4accb664a5763547e678341764d0d3c22044ca
Vinicius Reis <vinicius.reis@ligainvest.com.br>
2023-03-09 16:28:30 -0300

    wip: base_router

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import uvicorn
 
 from src.routers.base.router import BaseRouter
 
-
 app = BaseRouter.register_routers()
 
 if __name__ == "__main__":
---
+++ src/routers/base/router.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Request
+
+from src.routers.credit_card.router import CreditCardRouter
+
+
+class BaseRouter:
+    __app = FastAPI(
+        title="Kubera API",
+        description="Credit Card Crud"
+    )
+
+    @classmethod
+    def __include_credit_card_router(cls):
+        credit_card_router = CreditCardRouter.get_credit_card_router()
+        cls.__app.include_router(credit_card_router)
+        return cls.__app
+
+    @classmethod
+    def register_routers(cls):
+        cls.__include_credit_card_router()
+        return cls.__app
+
+    @staticmethod
+    @__app.middleware("http")
+    async def middleware_response(request: Request, call_next: callable):
+        middleware_service_response = await BaseRouter.__add_process_time_header(
+            request=request, call_next=call_next
+        )
+        return middleware_service_response
+
+    @staticmethod
+    async def __add_process_time_header(request: Request, call_next: callable):
+        response = None
+
+        try:
+            response = await call_next(request)
+
+        except Exception as ex:
+            pass
+            #  logging here
+            # response = "pass"
+
+        finally:
+            return response
+
+
+
