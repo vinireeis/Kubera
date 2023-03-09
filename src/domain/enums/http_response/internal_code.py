2c37b3b7a59ddea89c4065132c59339533720c3b
Vinicius Reis <vinicius.reis@ligainvest.com.br>
2023-03-09 16:27:45 -0300

    feat: create internal_codes

---
+++ src/domain/enums/http_response/internal_code.py
@@ -0,0 +1,10 @@
+# Standards
+from enum import IntEnum
+
+
+class InternalCode(IntEnum):
+    SUCCESS = 0
+    INVALID_PARAMS = 10
+    INVALID_AUTHENTICATION = 30
+    UNAUTHORIZED = 40
+    DATA_VALIDATION_ERROR = 50
