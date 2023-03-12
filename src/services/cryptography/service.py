f09d46cf3dd1815c8902d5692607a461a3c19650
Vinicius Reis <vinicius.reis@ligainvest.com.br>
2023-03-12 01:24:59 -0300

    feat: implement criptography service

---
+++ src/services/cryptography/service.py
@@ -0,0 +1,23 @@
+from cryptography.fernet import Fernet
+from decouple import config
+
+from src.domain.models.credit_card.model import CreditCardModel
+from src.domain.validators.credit_card.validator import CreditCardValidator
+
+
+class CryptographyService:
+    fernet_crypter = Fernet(config("KUBERA_ENCRYPTION_KEY"))
+
+    @classmethod
+    def encrypt_number(cls, payload: CreditCardValidator) -> bytes:
+        data = payload.number.encode()
+        number_encrypted = cls.fernet_crypter.encrypt(data=data)
+
+        return number_encrypted
+
+    @classmethod
+    def decrypt_number(cls, payload: CreditCardModel) -> str:
+        token = payload.number_encoded  # TODO: ajustar com o obj de retorno do banco
+        decrypted_number = cls.fernet_crypter.decrypt(token=token)
+        number = decrypted_number.decode()
+        return number
