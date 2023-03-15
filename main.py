21bed4ffe68f821fb1e213d039d2f7c765ff6b38
Vinicius Reis <v_reis@outlook.com.br>
2023-03-15 13:27:26 -0300

    fix: loglevel

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from src.routers.base.router import BaseRouter
 app = BaseRouter.register_routers()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000, log_level=10)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
